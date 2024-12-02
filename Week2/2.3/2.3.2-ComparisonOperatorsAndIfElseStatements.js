d7195f8a927acaa35e5d089ec0b754dc65b9bfea
olerk16 <netlocolerk16@hotmail.com>
2024-12-02 15:35:43 -0600

    finished activities to 2.3.4

---
+++ Week2/2.3/2.3.2-ComparisonOperatorsAndIfElseStatements.js
@@ -0,0 +1,25 @@
+let age = 16;
+
+// Write an If Else statement
+
+
+if (age >= 26) {
+console.log("Renting a car is more affordable.")
+} else {
+console.log("Renting a car is very expensive.")
+}
+// if age is greater than or equal to 25, 
+
+if (age >= 25) {
+console.log("Renting a car is more affordable.")
+} else if (age >= 18 ) {
+console.log("Renting a car is very expensive.")
+} else {
+console.log("You cannot legally rent a car.")
+}
+// if age is greater than or equal to 25,
+// console.log "Renting a car is more affordable."
+// if age is greater than or equal to 18
+// console.log "Renting a car is very expensive."
+// if age is less than 18
+// console.log "You cannot legally rent a car."
