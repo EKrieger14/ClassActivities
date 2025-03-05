e2ca09fe3114819ff731f2d95ca8621e2c072954
Michael Webb <mjwebb1993@yahoo.com>
2025-03-04 22:04:02 -0600

    Completed 4.2

---
+++ Week4/4.2/4.2.1-SelectingDOMElements.js
@@ -0,0 +1,29 @@
+// select all unordered list (ul) elements
+// getElementsByTagName
+document.getElementsByTagName("ul");
+
+// querySelectorAll
+document.querySelectorAll("ul");
+
+// select all elements with the class "class-week"
+// getElementByClassName
+document.getElementsByClassName("class-week");
+document.getElementsByClassName("class-week");
+
+// querySelectorAll
+document.querySelectorAll(".class-week");
+
+// select all elements with the class "nav-bar"
+// getElementByClassName
+document.getElementsByClassName("nav-bar");
+
+// querySelectorAll
+document.querySelectorAll(".nav-bar");
+
+// select the element with the id "dog-picture" and save it to a variable
+// getElementByID
+let example = document.getElementById("dog-picture").parentElement;
+console.log(example);
+let elementDogPicture = document.getElementById("dog-picture");
+// querySelector
+let queryDogPicute = document.querySelector("#dog-picture");
---
+++ Week4/4.2/index.js
@@ -0,0 +1,3 @@
+console.log(document.querySelector("#dog-picture"));
+console.log(document.querySelectorAll("li"));
+console.log(document.getElementsByClassName("class-week"));
