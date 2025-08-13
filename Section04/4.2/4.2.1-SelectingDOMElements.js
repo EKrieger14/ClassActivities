b2fec788a34a77cce9c3818645e5ae9d8c8aa4eb
Michael Webb <mjwebb1993@yahoo.com>
2025-08-12 21:48:06 -0500

    Through 4.2.3

---
+++ Section04/4.2/4.2.1-SelectingDOMElements.js
@@ -0,0 +1,13 @@
+// select all unordered list (ul) elements
+let ulQS = document.querySelectorAll("ul");
+let ulElements = document.getElementsByTagName("ul");
+console.log(ulQS);
+console.log(ulElements);
+
+// select all elements with the class "class-week"
+document.querySelectorAll(".class-week");
+// select all elements with the class "nav-bar"
+document.getElementsByClassName("nav-bar");
+// select the element with the id "dog-picture" and save it to a variable
+let dogPic = document.querySelector("#dog-picture");
+console.log(dogPic);
---
+++ Section04/4.2/index.js
@@ -0,0 +1,4 @@
+console.log("I am printed from index.js");
+console.log(document.querySelector("body").children);
+console.log(document.querySelector("body").textContent);
+// document.querySelector("body").innerHTML = "<h6>I am a new H1 element</h6>";
